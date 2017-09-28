2453821d1f823bc5aba5acc307c26286d4a0e3da
Brinnyman <ShadowBlade@ShadowBlade.local>
2017-09-28 15:12:39 +0200

    Created a seperate command file. on_message check if command in the dict. Then return command as embed.

---
+++ Commands.py
@@ -0,0 +1,170 @@
+import discord
+import random
+from experience import Experience
+experience = Experience()
+
+
+def embed_command():
+    def decorator(function):
+        async def wrapper(*args):
+            client = args[0]
+            message = args[2]
+            embed = await function(*args)
+            await client.send_message(message.channel, embed=embed)
+        return wrapper
+    return decorator
+
+
+@embed_command()
+async def ping(client, author, message):
+    msg = 'Pong!'
+    Embed = discord.Embed(description=msg, colour=0x42eef4, title="")
+    Embed.set_author(name=author.name, icon_url=author.avatar_url)
+    print(Embed.to_dict())
+    return Embed
+
+
+async def presence(client, author, message):
+    state = message.content.split(' ', 1)[-1]
+    if state == message.content:
+        await client.change_presence(game=None)
+    else:
+        await client.change_presence(game=discord.Game(name=state, status=None, afk=False))
+
+
+@embed_command()
+async def flip(client, author, message):
+    coin = random.randint(1, 2)
+    msg = 'Flipping a coin!!'
+    Embed = discord.Embed(description=msg, colour=0x42eef4, title="")
+    Embed.set_author(name=author.name, icon_url=author.avatar_url)
+
+    if coin == 1:
+        msg = 'You flipped Heads'
+        Embed = discord.Embed(description=msg, colour=0x42eef4, title="")
+        Embed.set_author(name=author.name, icon_url=author.avatar_url)
+    if coin == 2:
+        msg = 'You flipped tails'
+        Embed = discord.Embed(description=msg, colour=0x42eef4, title="")
+        Embed.set_author(name=author.name, icon_url=author.avatar_url)
+    print(Embed.to_dict())
+    return Embed
+
+
+@embed_command()
+async def profile(client, author, message):
+    mentioned = message.mentions[0]
+
+    Embed = discord.Embed(
+        color=mentioned.color,
+    )
+    Embed.set_author(
+        name="User Info: " + mentioned.name + "#" + mentioned.discriminator,
+    )
+    Embed.add_field(
+        name="ID",
+        value=mentioned.id,
+        inline=True
+    )
+    Embed.add_field(
+        name="Nickname",
+        value=mentioned.display_name,
+        inline=True
+    )
+    Embed.add_field(
+        name="Status",
+        value=mentioned.status,
+        inline=True
+    )
+    Embed.add_field(
+        name="Playing",
+        value=mentioned.game,
+        inline=True
+    )
+    Embed.add_field(
+        name="Role",
+        value=mentioned.top_role,
+        inline=True
+    )
+    Embed.add_field(
+        name="Avatar",
+        value="[Full size](" + mentioned.avatar_url + ")",
+        inline=True
+    )
+    Embed.add_field(
+        name="Experience points",
+        value='100',
+        inline=True
+    )
+    Embed.add_field(
+        name="Account Created",
+        value=str(mentioned.created_at).split('.', 1)[0],
+        inline=True
+    )
+    Embed.add_field(
+        name="Join Date",
+        value=str(mentioned.joined_at).split('.', 1)[0],
+        inline=True
+    )
+    Embed.set_thumbnail(
+        url=mentioned.avatar_url
+    )
+    print(Embed.to_dict())
+    return Embed
+
+
+@embed_command()
+async def color(client, author, message):
+    role_color = message.content.split(' ', 1)[-1]
+    role_colorint = int(message.content.split(' ', 1)[-1], 16)
+    role = discord.utils.get(author.server.roles, name=author.name)
+    if role is None:
+        role = await client.create_role(message.server, name=message.author.name, color=discord.Colour(role_colorint), hoist=False, mentionable=False)
+        position = None
+        for i in message.server.roles:
+            if i.name.lower() == 'member':
+                position = i
+        await client.move_role(message.server, role, position=position.position)
+        await client.add_roles(message.author, role)
+    elif role in message.server.roles:
+        await client.edit_role(message.server, role, colour=discord.Colour(role_colorint))
+
+    msg = 'Changed your profile color to '+str(role_color)+' !!'
+    Embed = discord.Embed(description=msg, colour=0x42eef4, title="")
+    Embed.set_author(name=author.name, icon_url=author.avatar_url)
+    print(Embed.to_dict())
+    return Embed
+
+
+@embed_command()
+async def showxp(client, author, message):
+    mentioned = message.mentions[0]
+    msg = '{} currently has {} experience points!!'.format(mentioned.name, experience.getxp(mentioned))
+    Embed = discord.Embed(description=msg, colour=0x42eef4, title="")
+    Embed.set_author(name=author.name, icon_url=author.avatar_url)
+    print(Embed.to_dict())
+    return Embed
+
+
+@embed_command()
+async def givexp(client, author, message):
+    mentioned = message.mentions[0]
+    amount = message.content.split(' ', 2)[2]
+    experience.setxp(mentioned, amount)
+    msg = mentioned.name + '#' + mentioned.discriminator + ' received ' + amount + ' experience points!!'
+    Embed = discord.Embed(description=msg, colour=0x42eef4, title="")
+    Embed.set_author(name=author.name, icon_url=author.avatar_url)
+    print(Embed.to_dict())
+    return Embed
+
+
+@embed_command()
+async def removexp(client, author, message):
+    mentioned = message.mentions[0]
+    amount = message.content.split(' ', 2)[2]
+    experience.removexp(mentioned, amount)
+    msg = mentioned.name + '#' + mentioned.discriminator + ' lost ' + amount + ' experience points!!'
+    Embed = discord.Embed(description=msg, colour=0x42eef4, title="")
+    Embed.set_author(name=author.name, icon_url=author.avatar_url)
+    print(Embed.to_dict())
+    return Embed
--- bot.py
+++ bot.py
@@ -2,12 +2,14 @@ import discord
 import asyncio
 from discord.ext import commands
 from config import Configuration
-from validation import validation
-from experience import Experience
-import random
+import Commands
+
+# from validation import validation
+# @validation()
+# msg = await bot.say(embed=embed)
+# await delete_message(5, ctx.message, msg)
 
 config = Configuration()
-experience = Experience()
 bot = commands.Bot(command_prefix=config.prefix)
 
 
@@ -33,181 +35,30 @@ async def on_command_error(exception, context):
                     \nType !help [command] to get help."""
     else:
         msg = "inconnu... " + str(type(exception)) + str(exception)
-        print(msg)
+
+    print(msg)
     msg1 = await bot.send_message(context.message.channel, msg)
     await asyncio.sleep(5)
     await bot.delete_message(msg1)
 
 
-async def delete_message(time, invoke, response):
-    await asyncio.sleep(time)
-    await bot.delete_message(invoke)
-    await bot.delete_message(response)
+command_list = {
+    "!ping": Commands.ping,
+    "!presence": Commands.presence,
+    "!flip": Commands.flip,
+    "!profile": Commands.profile,
+    "!color": Commands.color,
+    "!showxp": Commands.showxp,
+    "!givexp": Commands.givexp,
+    "!removexp": Commands.removexp
+    }
 
 
-# mod commands
-@bot.command(pass_context=True)
-@validation()
-async def ping(ctx):
-    msg = await bot.say('Pong!')
-    await delete_message(5, ctx.message, msg)
-
-
-@bot.command(pass_context=True)
-@validation()
-async def presence(ctx, message):
-    await bot.delete_message(ctx.message)
-    if message == 'stop':
-        await bot.change_presence(game=None)
-    else:
-        await bot.change_presence(game=discord.Game(name=message, status=None, afk=False))
-
-
-# member commands
-@bot.command(pass_context=True)
-@validation()
-async def color(ctx, message):
-    role_color = int(message, 16)
-    role = discord.utils.get(ctx.message.server.roles, name=ctx.message.author.name)
-    if role is None:
-        role = await bot.create_role(ctx.message.server, name=ctx.message.author.name, color=discord.Colour(role_color), hoist=False, mentionable=False)
-        position = None
-        for i in ctx.message.server.roles:
-            if i.name.lower() == 'member':
-                position = i
-        await bot.move_role(ctx.message.server, role, position=position.position)
-        await bot.add_roles(ctx.message.author, role)
-    elif role in ctx.message.server.roles:
-        await bot.edit_role(ctx.message.server, role, colour=discord.Colour(role_color))
-
-    msg = await bot.say('Changed your profile color to '+message+' !!')
-    await delete_message(5, ctx.message, msg)
-
-
-@bot.command(pass_context=True)
-@validation()
-async def flip(ctx):
-    coin = random.randint(1, 2)
-    msg = await bot.say('Flipping a coin!!')
-
-    await asyncio.sleep(2)
-    if coin == 1:
-        experience.setxp(ctx.message.author, 5)
-        msg2 = await bot.edit_message(msg, 'You flipped Heads and won 5 experience points!!')
-    if coin == 2:
-        experience.removexp(ctx.message.author, 5)
-        msg2 = await bot.edit_message(msg, 'You flipped Tails and lost 5 experience points!!')
-
-    await delete_message(5, ctx.message, msg2)
-
-
-@bot.command(pass_context=True)
-@validation()
-async def profile(ctx):
-    mentioned = ctx.message.mentions[0]
-
-    embed = discord.Embed(
-        color=mentioned.color,
-    )
-    embed.set_author(
-        name="User Info: " + mentioned.name + "#" + mentioned.discriminator,
-    )
-    embed.add_field(
-        name="ID",
-        value=mentioned.id,
-        inline=True
-    )
-    embed.add_field(
-        name="Nickname",
-        value=mentioned.display_name,
-        inline=True
-    )
-    embed.add_field(
-        name="Status",
-        value=mentioned.status,
-        inline=True
-    )
-    embed.add_field(
-        name="Playing",
-        value=mentioned.game,
-        inline=True
-    )
-    embed.add_field(
-        name="Role",
-        value=mentioned.top_role,
-        inline=True
-    )
-    embed.add_field(
-        name="Avatar",
-        value="[Full size](" + mentioned.avatar_url + ")",
-        inline=True
-    )
-    embed.add_field(
-        name="Experience points",
-        value=experience.getxp(mentioned),
-        inline=True
-    )
-    embed.add_field(
-        name="Account Created",
-        value=str(mentioned.created_at).split('.', 1)[0],
-        inline=True
-    )
-    embed.add_field(
-        name="Join Date",
-        value=str(mentioned.joined_at).split('.', 1)[0],
-        inline=True
-    )
-    embed.set_thumbnail(
-        url=mentioned.avatar_url
-    )
-    msg = await bot.say(embed=embed)
-    await delete_message(15, ctx.message, msg)
-
-
-@bot.command(pass_context=True)
-async def givexp(ctx, amount):
-    mentioned = ctx.message.mentions[0]
-    experience.setxp(mentioned, amount)
-
-    embed = discord.Embed(
-        color=mentioned.color,
-    )
-    embed.set_author(
-        name=mentioned.name + '#' + mentioned.discriminator + ' received ' + amount + ' experience points!!',
-    )
-    msg = await bot.say(embed=embed)
-    await delete_message(5, ctx.message, msg)
-
-
-@bot.command(pass_context=True)
-async def removexp(ctx, amount):
-    mentioned = ctx.message.mentions[0]
-    experience.removexp(mentioned, amount)
-
-    embed = discord.Embed(
-        color=mentioned.color,
-    )
-    embed.set_author(
-        name=mentioned.name + '#' + mentioned.discriminator + ' lost ' + amount + ' experience points!!',
-    )
-    msg = await bot.say(embed=embed)
-    await delete_message(5, ctx.message, msg)
-
-
-@bot.command(pass_context=True)
-async def showxp(ctx):
-    # mentioned = ctx.message.author
-    mentioned = ctx.message.mentions[0]
-
-    embed = discord.Embed(
-        color=mentioned.color,
-    )
-    embed.set_author(
-        # name='You currently have {} experience points in channel #{}'.format(member.getxp(mentioned), ctx.message.channel),
-        name='{} currently has {} experience points in channel #{}'.format(mentioned.name, experience.getxp(mentioned), ctx.message.channel),
-    )
-    msg = await bot.say(embed=embed)
-    await delete_message(5, ctx.message, msg)
-
+@bot.event
+async def on_message(message):
+    author = message.author
+    for command, function in command_list.items():
+        if message.content.startswith(command):
+            await function(bot, author, message)
 
 bot.run(config.token)

8ebbdd4b8bd19a52cd8f42778324182c82a89adf
Brinnyman <ShadowBlade@ShadowBlade.local>
2017-10-03 11:46:50 +0200

    game; added permit checks, showxp displays the users rank, removed old permissions message.

--- cogs/game.py
+++ cogs/game.py
@@ -11,48 +11,42 @@ class Game():
     def __init__(self, bot):
         self.bot = bot
 
+    @permit.check()
     @commands.command(pass_context=True)
     async def showxp(self, ctx, member: discord.Member):
         """Displays a members' experience points."""
-        msg = '{} currently has {} experience points!!'.format(member.name, experience.getxp(member))
-        Embed = discord.Embed(description=msg, colour=0x42eef4)
-        message = await self.bot.send_message(ctx.message.channel, embed=Embed)
+        # msg = '{} currently has {} experience points!!'.format(member.name, experience.getxp(member))
+        msg = '{} currently has {} experience points and is a {}!!'.format(member.name, experience.getxp(member), experience.getrank(member))
+        Embed = discord.Embed(description=msg, color=ctx.message.server.me.color)
+        Embed.set_footer(text='Invoked by: ' + ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
+        await self.bot.send_message(ctx.message.channel, embed=Embed)
         await asyncio.sleep(5)
-        await self.bot.delete_message(message)
         await self.bot.delete_message(ctx.message)
 
+    @permit.check()
     @commands.command(pass_context=True)
     async def givexp(self, ctx, member: discord.Member, amount: str):
         """Gives a member experience points."""
-        if(permit.check_command(ctx.message.author, ctx.message.content.split(' ', 1)[0][1:])):
-            experience.setxp(member, amount)
-            msg = member.name + ' received ' + amount + ' experience points!!'
-            Embed = discord.Embed(description=msg, colour=0x42eef4)
-            message = await self.bot.send_message(ctx.message.channel, embed=Embed)
-        else:
-            msg = 'You dont have the right permission!!'
-            Embed = discord.Embed(description=msg, colour=0x42f4a1)
-            message = await self.bot.send_message(ctx.message.channel, embed=Embed)
+        experience.setxp(member, amount)
+        msg = member.name + ' received ' + amount + ' experience points!!'
+        Embed = discord.Embed(description=msg, color=ctx.message.server.me.color)
+        Embed.set_footer(text='Invoked by: ' + ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
+        await self.bot.send_message(ctx.message.channel, embed=Embed)
 
         await asyncio.sleep(5)
-        await self.bot.delete_message(message)
         await self.bot.delete_message(ctx.message)
 
+    @permit.check()
     @commands.command(pass_context=True)
     async def removexp(self, ctx, member: discord.Member, amount: str):
         """Removes experience points from a members."""
-        if(permit.check_command(ctx.message.author, ctx.message.content.split(' ', 1)[0][1:])):
-            experience.removexp(member, amount)
-            msg = member.name + ' lost ' + amount + ' experience points!!'
-            Embed = discord.Embed(description=msg, colour=0x42eef4)
-            message = await self.bot.send_message(ctx.message.channel, embed=Embed)
-        else:
-            msg = 'You dont have the right permission!!'
-            Embed = discord.Embed(description=msg, colour=0x42f4a1)
-            message = await self.bot.send_message(ctx.message.channel, embed=Embed)
+        experience.removexp(member, amount)
+        msg = member.name + ' lost ' + amount + ' experience points!!'
+        Embed = discord.Embed(description=msg, color=ctx.message.server.me.color)
+        Embed.set_footer(text='Invoked by: ' + ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
+        await self.bot.send_message(ctx.message.channel, embed=Embed)
 
         await asyncio.sleep(5)
-        await self.bot.delete_message(message)
         await self.bot.delete_message(ctx.message)
 
 

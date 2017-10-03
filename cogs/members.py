2f07cc8f2bd309e86e6f75fd4f8b170a8afb489a
Brinnyman <ShadowBlade@ShadowBlade.local>
2017-10-03 10:07:03 +0200

    white spacing

--- cogs/members.py
+++ cogs/members.py
@@ -116,7 +116,7 @@ class Members():
     async def roll(self, ctx, dice: str):
         """Rolls a dice in NdN format."""
         rolls, limit = map(int, dice.split('d'))
-        msg = ctx.message.author.name + ' rolled: ' + dice + ' and got ' +', '.join(str(random.randint(1, limit)) for r in range(rolls))
+        msg = ctx.message.author.name + ' rolled: ' + dice + ' and got ' + ', '.join(str(random.randint(1, limit)) for r in range(rolls))
         Embed = discord.Embed(description=msg, colour=0x42eef4)
         Embed.set_footer(text='Invoked by: ' + ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
         await self.bot.send_message(ctx.message.channel, embed=Embed)
